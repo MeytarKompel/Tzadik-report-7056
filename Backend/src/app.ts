89a1a45f0d0a5836bcc753f606b4af77705c5992
Meytar <meytarkompell@gmail.comgit config --global user.email meytarkompell@gmail.comgit config --global user.email meytarkompell@gmail.com>
2026-03-17 15:59:59 +0200

    app.ts update

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -5,14 +5,14 @@ import express from "express";
 import cors from "cors";
 import catchAll from "./3-middleware/catch-all";
 import routeNotFound from "./3-middleware/route-not-found";
-import controller from "./6-controllers/device-controller";
+import deviceController from "./6-controllers/device-controller";
 import config from "./2-utils/config";
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
-server.use("/api", controller);
+server.use("/api", deviceController);
 server.use("*", routeNotFound);
 server.use(catchAll);
 
