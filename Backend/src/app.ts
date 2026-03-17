57139fcd25819fd5126bfb2d7b76b36341ae6f8c
Meytar <meytarkompell@gmail.comgit config --global user.email meytarkompell@gmail.comgit config --global user.email meytarkompell@gmail.com>
2026-03-17 19:16:15 +0200

    adding controllers to app.ts

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -7,12 +7,16 @@ import catchAll from "./3-middleware/catch-all";
 import routeNotFound from "./3-middleware/route-not-found";
 import deviceController from "./6-controllers/device-controller";
 import config from "./2-utils/config";
+import userController from "./6-controllers/user-controller";
+import reportController from "./6-controllers/report-controller";
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 server.use("/api", deviceController);
+server.use("/api", userController);
+server.use("/api", reportController);
 server.use("*", routeNotFound);
 server.use(catchAll);
 
