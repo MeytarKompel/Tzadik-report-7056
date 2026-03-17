40201dc9a33879cce9f2c4ea61c4cf6cb18229c5
Meytar <meytarkompell@gmail.comgit config --global user.email meytarkompell@gmail.comgit config --global user.email meytarkompell@gmail.com>
2026-03-17 20:34:02 +0200

    change to .env directory

--- Backend/src/2-utils/config.ts
+++ Backend/src/2-utils/config.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("Loaded MONGO_URI:", process.env.MONGO_URI);
+
 class Config {
     public connectionString = process.env.MONGO_URI as string;
     public port = Number(process.env.PORT) || 3001;
