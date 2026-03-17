63140de73c13559c61443265482b5301745103fa
Meytar <meytarkompell@gmail.comgit config --global user.email meytarkompell@gmail.comgit config --global user.email meytarkompell@gmail.com>
2026-03-17 15:12:12 +0200

    config and .env update

--- Backend/src/2-utils/config.ts
+++ Backend/src/2-utils/config.ts
@@ -1,8 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 class Config {
-    public connectionString = "mongodb://127.0.0.1:27017/_____"; // Fill in the blank (database name)...
-    public port = 3001;
+    public connectionString = process.env.MONGO_URI as string;
+    public port = Number(process.env.PORT) || 3001;
 }
 
 const config = new Config();
-
 export default config;
